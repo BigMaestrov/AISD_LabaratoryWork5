687a3e0d0d77246cda4a6f98e680d787bdc4f7a2
BigMaestrov <nikita.egorov40k@gmail.com>
2022-05-05 11:23:22 +0400

    Add time log.

--- src/main/java/com/example/aisd_labaratorywork5/HelloApplication.java
+++ src/main/java/com/example/aisd_labaratorywork5/HelloApplication.java
@@ -3,9 +3,11 @@ package com.example.aisd_labaratorywork5;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -13,10 +15,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
         Scene scene = new Scene(fxmlLoader.load(), 700, 600);
         stage.setTitle("AISD Labaratory 5");
         stage.setScene(scene);
@@ -90,6 +93,5 @@ public class HelloApplication extends Application {
                 System.out.println("Ошибка ввода");
             }
         }*/
-
     }
 }
--- src/main/java/com/example/aisd_labaratorywork5/HelloController.java
+++ src/main/java/com/example/aisd_labaratorywork5/HelloController.java
@@ -5,8 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-    Queue queue = new Queue();
-    Stack stack = new Stack();
+    Queue queue;
+    Stack stack;
+    double time;
+
     @FXML
     private Button startButton;
 
@@ -20,7 +22,23 @@ public class HelloController {
     private TextField lifotext;
 
     @FXML
+    private TextField datatext;
+
+    @FXML
     protected void clickAdd() {
+        //Инициализация очереди и стека
+        if (queue == null && stack == null) {
+
+            time = System.nanoTime();
+            queue = new Queue();
+            time = System.nanoTime() - time;
+            datatext.appendText("Queue init time: " + time + " | ");
+
+            time = System.nanoTime();
+            stack = new Stack();
+            time = System.nanoTime() - time;
+            datatext.appendText("Stack init time: " + time + " | ");
+        }
         fifotext.clear();
         lifotext.clear();
         double buff;
@@ -34,12 +52,20 @@ public class HelloController {
         if (array.length > 0 && incomingDataCorrect) {
             for (int i = 0; i < array.length; i++) {
                 buff = Double.parseDouble(array[i]);
+                //Добавление в очередь и измерение времени
+                time = System.nanoTime();
                 queue.addNode(buff);
+                time = System.nanoTime() - time;
+                datatext.appendText("Queue add time: " + time + " | ");
+                //Добавление в стек и измерение времени
+                time = System.nanoTime();
                 stack.addNode(buff);
+                time = System.nanoTime() - time;
+                datatext.appendText("Stack add time: " + time + " | ");
             }
             fifotext.appendText((queue.getStringQueue()));
             lifotext.appendText((stack.getStringStack()));
-        }else{
+        } else {
             System.err.println("uncorrected data");
             input.clear();
         }
@@ -49,8 +75,19 @@ public class HelloController {
     protected void clickDelete() {
         fifotext.clear();
         lifotext.clear();
+
+        //Удаление из очереди и измерение времени
+        time = System.nanoTime();
         queue.deleteNode();
+        time = System.nanoTime() - time;
+        datatext.appendText("Queue delete time: " + time + " | ");
+
+        //Удаление из стека и измерение времени
+        time = System.nanoTime();
         stack.deleteNode();
+        time = System.nanoTime() - time;
+        datatext.appendText("Stack delete time: " + time + " | ");
+
         fifotext.appendText((queue.getStringQueue()));
         lifotext.appendText((stack.getStringStack()));
     }
